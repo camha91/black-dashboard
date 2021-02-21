848ad165ea607d28e845b3456db64dfb676a2caa
camha91 <crystalhanguyen@gmail.com>
2021-02-20 21:35:05 -0800

    Create buttons for sizes and styles

--- src/views/components/Buttons.jsx
+++ src/views/components/Buttons.jsx
@@ -19,12 +19,13 @@ const Buttons = (props) => {
               <CardTitle>Pick your Color</CardTitle>
             </CardHeader>
             <CardBody>
-              <Button className="btn btn-primary">Primary</Button>
-              <Button className="btn btn-secondary">Secondary</Button>
-              <Button className="btn btn-success">Success</Button>
-              <Button className="btn btn-info">Info</Button>
-              <Button className="btn btn-warning">Warning</Button>
-              <Button className="btn btn-danger">Danger</Button>
+              <Button color="primary">Primary</Button>
+              <Button color="secondary">Secondary</Button>
+              <Button color="success">Success</Button>
+              <br />
+              <Button color="info">Info</Button>
+              <Button color="warning">Warning</Button>
+              <Button color="danger">Danger</Button>
             </CardBody>
           </Col>
           <Col sm="6">
@@ -32,12 +33,64 @@ const Buttons = (props) => {
               <CardTitle>Buttons with Label</CardTitle>
             </CardHeader>
             <CardBody>
-              <Button className="btn btn-primary">Primary</Button>
-              <Button className="btn btn-secondary">Secondary</Button>
-              <Button className="btn btn-success">Success</Button>
-              <Button className="btn btn-info">Info</Button>
-              <Button className="btn btn-warning">Warning</Button>
-              <Button className="btn btn-danger">Danger</Button>
+              <Button color="default">
+                <i className="icon-minimal-left" /> Left
+              </Button>
+              <Button color="default">
+                Right <i className="icon-minimal-right" />
+              </Button>
+              <Button color="success">
+                <i className="tim-icons icon-check-2" /> Success
+              </Button>
+              <br />
+              <Button color="info">
+                <i className="tim-icons icon-alert-circle-exc" /> Info
+              </Button>
+              <Button color="warning">
+                <i className="tim-icons icon-time-alarm" /> Warning
+              </Button>
+              <Button color="danger">
+                <i className="tim-icons icon-simple-remove" /> Danger
+              </Button>
+            </CardBody>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="6">
+            <CardHeader>
+              <CardTitle>Pick your Size</CardTitle>
+            </CardHeader>
+            <CardBody>
+              <Button color="primary" size="sm">
+                Small
+              </Button>
+              <Button color="primary">Regular</Button>
+              <Button color="primary" size="lg">
+                Large
+              </Button>
+            </CardBody>
+          </Col>
+          <Col sm="6">
+            <CardHeader>
+              <CardTitle>Pick your Style</CardTitle>
+            </CardHeader>
+            <CardBody>
+              <Button color="primary">Default</Button>
+              <Button className="btn-round" color="primary">
+                Round
+              </Button>
+              <Button className="btn-round" color="primary">
+                <i className="icon-heart-2" /> with icon
+              </Button>
+              <Button className="btn-round btn-icon" color="primary">
+                <i className="icon-heart-2" /> Heart icon
+              </Button>
+              <Button className="btn-simple" color="primary">
+                Simple
+              </Button>
+              <Button className="btn-link" color="primary">
+                Link
+              </Button>
             </CardBody>
           </Col>
         </Row>
